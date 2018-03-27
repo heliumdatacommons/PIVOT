c5892175ac7b49ae86d0ae761395db534f0f5cb4
dcvan <dcvan@cs.unc.edu>
2018-03-27 11:07:09 -0400

    moved Dockerfiles to a dedicated project

--- docker/chronos/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-export LIBPROCESS_IP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
-env MESOS_NATIVE_JAVA_LIBRARY=/usr/lib/libmesos.so \
-    java -cp $HOME/chronos*.jar org.apache.mesos.chronos.scheduler.Main $@
